fb06dbfc1e35b6187fc2bf423b27d75878ad83a1
Kantonov <kk_antonov@abv.bg>
2017-07-28 17:14:24 +0300

    Small fix

--- config/app.config.js
+++ config/app.config.js
@@ -2,11 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.set('view engine", "pug');
+app.set('view engine', 'pug');
 // app.set('views', constants.rootPath + '../views');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/static", express.static("public');
+app.use('/static', express.static('public'));
 
 module.exports = app;
