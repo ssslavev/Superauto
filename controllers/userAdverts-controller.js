4ec2796fbd234648782dc5e1194ce7308592fdf2
Kantonov <kk_antonov@abv.bg>
2017-07-30 03:15:11 +0300

    Fixed adverts for logged users
    TODO: fix the UI if theres any time left

--- controllers/userAdverts-controller.js
+++ controllers/userAdverts-controller.js
@@ -1,11 +1,22 @@
 module.exports = (data) => {
     return {
         userAdverts(req, res) {
-            if (req.isAuthenticated()) {
-                return res.render('users/adverts', {
-                    user: req.user });
+            if (!req.isAuthenticated()) {
+                return res.redirect('/login');
             }
-            return res.redirect('/login');
+            return Promise
+            .resolve(data.advertsData.getAdvertsByUser(req.user._id))
+            .then((reqAdverts) => {
+                console.log('boo');
+                console.log(reqAdverts);
+                return res.render('users/adverts', {
+                    user: req.user,
+                    adverts: reqAdverts,
+                });
+            })
+            .catch((err) =>{
+                return console.log(err);
+            });
         },
     };
 };
